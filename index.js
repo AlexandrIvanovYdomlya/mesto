36778a3ba8903255819a8fb9b79533203376ce5c
Alexandr Ivanov <ivanovsunsunich@yandex.ru>
2022-03-13 19:21:22 +0300

    refactoring_second_review

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.input_name');
-let jobInput = document.querySelector('.input_description');
+let nameInput = document.querySelector('.popup-form__input-name');
+let jobInput = document.querySelector('.popup-form__input-description');
 let editButton = document.querySelector('.profile__edit-button');
 let saveButton = document.querySelector('.popup__edit-button');
 let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
+let body = document.querySelector('.body')
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -24,10 +25,12 @@ function formSubmitHandler(evt) {
 
 function closePopup(){
   popup.classList.remove('popup_opened')
+  body.classList.remove('body_popup')
 }
 
 function openPopup(){
   popup.classList.add('popup_opened')
+  body.classList.add('body_popup')
 
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
