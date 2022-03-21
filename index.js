0b6ae7bf9f879e199cd4d7c17113086d500f7f17
Alexandr Ivanov <ivanovsunsunich@yandex.ru>
2022-03-21 15:44:58 +0300

    5_sprint_start

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
 let body = document.querySelector('.body')
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
