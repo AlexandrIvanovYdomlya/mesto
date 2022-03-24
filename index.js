24b933acf88c3bfc007dc289d87d8e6814e26034
Alexandr Ivanov <ivanovsunsunich@yandex.ru>
2022-03-24 11:35:19 +0300

    card-array

--- index.js
+++ index.js
@@ -11,65 +11,85 @@ let closeButton = document.querySelector('.popup__close');
 let body = document.querySelector('.body')
 
 const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
 ];
 
+const cardsList = document.querySelector('#element-template');
+
+function renderCards(cardName, cardLink){
+  return`<li class="element">
+          <img class="element__photo" src="${cardLink}" alt="карачаевск">
+          <p class="element__name">${cardName}</p>
+          <button type="button" class="button element__like"></button>
+        </li>`
+}
+
+cardsList.innerHTML = cardsList.innerHTML + cardsList.map
+
+//Заполнение формы профиля
 function formSubmitHandler(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
 
-  //profileName.textContent = profileName;
-  //profileDescription.textContent = profileDescription;
-
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-  
-  closePopup();
+    closePopup();
 }
 
+//Закрытие модального окна
 function closePopup(){
-  popup.classList.remove('popup_opened')
-  body.classList.remove('body_popup')
+    popup.classList.remove('popup_opened')
+    body.classList.remove('body_popup')
 }
 
+//Открытие модального окна
 function openPopup(){
+    popup.classList.add('popup_opened')
+    body.classList.add('body_popup')
+
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+}
+/*
+function openPopup(title, subtitle){
   popup.classList.add('popup_opened')
   body.classList.add('body_popup')
 
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileDescription.textContent;
-}
+  nameInput.value = title.textContent;
+  jobInput.value = subtitle.textContent;
+}*/
 
 function addCard(name, link){
-  const cardTemplate = document.querySelector('.element__name'/*'#element-template'*/).content;
-  const linkTemplate = document.querySelector('.'/*'#element-template'*/).content;
+    const cardTemplate = document.querySelector('.element__name'/*'#element-template'*/).content;
+    const linkTemplate = document.querySelector('.'/*'#element-template'*/).content;
 
 
 }
 
 closeButton.addEventListener('click', closePopup);
 editButton.addEventListener('click', openPopup);
+/*editButton.addEventListener('click', openPopup(profileName, profileDescription));*/
 formElement.addEventListener('submit', formSubmitHandler);
